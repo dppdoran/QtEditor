62e1da8f614e3b6f079176ba6c03f03491c79f97
D P Duck <dppdoran@outlook.com>
2021-06-04 17:43:28 +0200

    And then there was text

--- ed.py
+++ ed.py
@@ -8,8 +8,28 @@ class Main(QtWidgets.QMainWindow):
         self.init_ui()
 
     def init_ui(self):
+        self.text = QtWidgets.QTextEdit(self)
+        self.setCentralWidget(self.text)
+        self.init_toolbar()
+        self.addToolBarBreak()
+        self.init_format_bar()
+        self.init_menubar()
+        self.statusbar = self.statusBar()
         self.setGeometry(100, 100, 1030, 800)
         self.setWindowTitle("Qt Editor")
+
+    def init_toolbar(self):
+        self.toolbar = self.addToolBar("Options")
+
+    def init_format_bar(self):
+        self.formatbar = self.addToolBar("Format")
+
+    def init_menubar(self):
+        menubar = self.menuBar()
+        file = menubar.addMenu("File")
+        edit = menubar.addMenu("Edit")
+        view = menubar.addMenu("View")
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
